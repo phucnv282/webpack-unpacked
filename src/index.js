ba83c10fe9100de1431c88cb80be7e11e3744106
phucnv282 <phucnv282@gmail.com>
2021-11-08 13:00:25 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import natureImg from "./assets/nature.jpg";
+
+const img = document.createElement("img");
+img.src = natureImg;
+document.body.appendChild(img);
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpe?g|svg)/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "images/",
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+  devServer: {
+    port: 3000,
+  },
+};
